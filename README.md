04cf74727c63e0b4af4acdcbd075fac92245151e
iawall <iawall@siue.edu>
2024-01-25 13:16:27 -0600

    added readme and list.py

---
+++ README.md
@@ -0,0 +1,2 @@
+Quiz 3 repository
+The program outputs the sum and factor of a number
---
+++ list.py
@@ -0,0 +1,12 @@
+i = 1
+sum = 0
+factorial = 1
+for i in range(1,6):
+    print(i)
+    sum += i
+    factorial *= i
+print()
+print("The sum is:")
+print(sum)
+print("The factorial is:")
+print(factorial)
